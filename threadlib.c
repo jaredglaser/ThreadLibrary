34ca466f48e4241ba10fc0b12318b3cae74d48ca
Jared Glaser <jared@glaserfamily.net>
2019-05-23 14:31:37 -0400

    passes T5 and T8

--- threadlib.c
+++ threadlib.c
@@ -9,6 +9,8 @@
 tcb *runningQueue = NULL;
 tcb *readyQueue = NULL;
 
+mbox *messageQueue;
+
 int value = 0;
 
 void t_init()
@@ -16,6 +18,9 @@ void t_init()
   runningQueue = malloc(sizeof(tcb));
   runningQueue->value = malloc(sizeof(ucontext_t));
   runningQueue->thread_id = 0;
+  mbox_create(&(messageQueue));
+  sem_init(&(messageQueue->mbox_sem), 0);
+
   getcontext(runningQueue->value); // let back be the context of main()
 }
 void t_shutdown()
@@ -53,9 +58,9 @@ void t_yield()
     {
       temp = temp->next;
     }
-    temp->next = runningHead; //add current running thread to the end of the ready queue
-    runningQueue = readyQueue; //make the running thread the head of the ready queue
-    readyQueue = readyQueue->next; //iterate ready queue
+    temp->next = runningHead;                             //add current running thread to the end of the ready queue
+    runningQueue = readyQueue;                            //make the running thread the head of the ready queue
+    readyQueue = readyQueue->next;                        //iterate ready queue
     swapcontext(runningHead->value, runningQueue->value); //swap em
   }
 }
@@ -134,11 +139,12 @@ void sem_wait(sem_t *sp)
       }
       temp->next = runningHead;
     }
-    if(readyQueue !=NULL){
-    runningQueue = readyQueue;     //put current running thread as ready thread
-    
+    if (readyQueue != NULL)
+    {
+      runningQueue = readyQueue; //put current running thread as ready thread
+
       readyQueue = readyQueue->next; //then iterate the ready queue
-    swapcontext(runningHead->value, runningQueue->value);
+      swapcontext(runningHead->value, runningQueue->value);
     }
   }
   sigrelse();
@@ -157,14 +163,16 @@ void sem_signal(sem_t *sp)
       sem->next = NULL;
     }
     tcb *temp = readyQueue;
-    if(temp!=NULL){
-    while (temp->next) 
+    if (temp != NULL)
     {
-      temp = temp->next;
+      while (temp->next)
+      {
+        temp = temp->next;
+      }
+      temp->next = sem; //add the head of the semaphore queue to the end of the ready queue
     }
-    temp->next = sem; //add the head of the semaphore queue to the end of the ready queue
-    }
-    else{
+    else
+    {
       readyQueue = sem;
     }
     sigrelse();
@@ -179,82 +187,157 @@ void sem_destroy(sem_t **sp)
 /* 
 *Create a mailbox pointed to by mb.
  */
-int mbox_create(mbox **mb){
-   *mb = malloc(sizeof(mbox));
-   (*mb)->msg = NULL;
-   (*mb)->mbox_sem = NULL;
-} 
+int mbox_create(mbox **mb)
+{
+  *mb = malloc(sizeof(mbox));
+  (*mb)->msg = NULL;
+  (*mb)->mbox_sem = NULL;
+}
 /* 
 * Destroy any state related to the mailbox pointed to by mb.
  */
-void mbox_destroy(mbox **mb){
+void mbox_destroy(mbox **mb)
+{
   //TODO: add implementation
 }
 /* 
 * Deposit message msg of length len into the mailbox pointed to by mb. 
 */
-void mbox_deposit(mbox *mb, char *msg, int len){
+void mbox_deposit(mbox *mb, char *msg, int len)
+{
   /*
   * Set up the messageNode to be added to the mailbox
   */
   messageNode *node = malloc(sizeof(messageNode));
   node->len = len;
-   node->message = malloc((sizeof(char)*len) + 1); //malloc space for the char arr
-   strcpy(node->message,msg); //copy it in
-   node->next = NULL; //next should be null since it will added to the end
-  node->sender = runningQueue->thread_id; //sender is current thread. Reciever will be added later
+  node->message = malloc((sizeof(char) * len) + 1); //malloc space for the char arr
+  strcpy(node->message, msg);                       //copy it in
+  node->next = NULL;                                //next should be null since it will added to the end
+  node->sender = runningQueue->thread_id;           //sender is current thread. Reciever will be added later
   /*
   * Add the messageNode to the mailbox
   */
-  if(mb->msg == NULL){ //add the message to the head
+  if (mb->msg == NULL)
+  { //add the message to the head
     mb->msg = node;
   }
-  else{ //we need to add it to the end
-    messageNode *temp = mb->msg; 
-    while(temp->next != NULL){
+  else
+  { //we need to add it to the end
+    messageNode *temp = mb->msg;
+    while (temp->next != NULL)
+    {
       temp = temp->next;
     }
-    temp -> next = node; //add it to the end
+    temp->next = node; //add it to the end
   }
 }
- /* 
+/* 
  * Withdraw the first message from the mailbox pointed to by mb into msg and set the message's length in len accordingly
  */
- void mbox_withdraw(mbox *mb, char *msg, int *len){
-   //get the first message, which should be the head
-   if(mb->msg == NULL){
-     *len = 0;
-     return;
-   }
-   else{
-     messageNode *node = mb->msg;
-     strcpy(msg,node->message); //put the message into the char pointer
-     *len = node->len; //give len the correct value
-     mb->msg = mb->msg->next; //iterate the queue
-   }
- }
-/* 
-*Send a message to the thread whose tid is tid. msg is the pointer to the start of the
-* message, and len specifies the length of the message in bytes. In your implementation, 
-*all messages are character strings. 
-*/
- void send(int tid, char *msg, int len){
-   
- }
+void mbox_withdraw(mbox *mb, char *msg, int *len)
+{
+  //get the first message, which should be the head
+  if (mb->msg == NULL)
+  {
+    *len = 0;
+    return;
+  }
+  else
+  {
+    messageNode *node = mb->msg;
+    strcpy(msg, node->message); //put the message into the char pointer
+    *len = node->len;           //give len the correct value
+    mb->msg = mb->msg->next;    //iterate the queue
+  }
+}
 
- /* 
- * Wait for and receive a message from another thread. The caller has to specify the 
- * sender's tid in tid, or sets tid to 0 if it intends to receive a message sent by any 
- * thread. If there is no "matching" message to receive, the calling thread waits (i.e., 
- * blocks itself). [A sending thread is responsible for waking up a waiting, receiving 
- * thread.] Upon returning, the message is stored starting at msg. The tid of the thread 
- * that sent the message is stored in tid, and the length of the message is stored in len.
- * The caller of receive() is responsible for allocating the space in which the message 
- * is stored. Even if more than one message awaits the caller, only one message is 
- * returned per call to receive(). Messages are received in the order in which they were 
- * sent. The caller will not resume execution until it has received a message (blocking 
- * receive). 
- */ 
-void receive(int *tid, char *msg, int *len){
+void send(int tid, char *msg, int len)
+{
+  struct messageNode *newMessage = (messageNode *)malloc(sizeof(messageNode));
+  struct messageNode *headMessage = messageQueue->msg;
+  
+  /*
+  * Deep copy the message to be added to the end of the queue
+  */
+  newMessage->message = malloc(len + 1);
+  strcpy(newMessage->message, msg);
+  newMessage->len = len;
+  newMessage->receiver = tid;
+  newMessage->sender = runningQueue->thread_id;
+  newMessage->next = NULL;
 
+  if (messageQueue->msg == NULL) //if it is null then set it as the first message in the queue
+  {
+    messageQueue->msg = newMessage;
+  }
+  else //otherwise put it at the end of the queue
+  {
+    while (headMessage->next)
+    {
+      headMessage = headMessage->next;
+    }
+    headMessage->next = newMessage;
+  }
+  sem_signal(messageQueue->mbox_sem);
+}
+
+void receive(int *tid, char *msg, int *len)
+{
+  struct messageNode *headMessage = messageQueue->msg;
+  struct messageNode *tmp;
+  sem_wait(messageQueue->mbox_sem);
+  if (headMessage == NULL)
+  {
+    len = 0;
+  }
+  else
+  {
+    //find if the first message is a message we want to recieve
+    if (((headMessage->receiver) == (runningQueue->thread_id)) && (headMessage->sender == *tid) || (*tid == 0)) 
+    {
+      if (*tid == 0) //handle changing tid to the message sender to pass the rest of T8
+      {
+        *tid = headMessage->sender;
+      }
+      strcpy(msg, headMessage->message); //populate msg
+      *len = headMessage->len; //populate len
+      if (headMessage != NULL) //iterate the queue
+      {
+        messageQueue->msg = headMessage->next;
+        freemsg(headMessage); //prevent leak
+      }
+      else
+      {
+        //otherwise we need to look throught the entire queue to find the message
+        while (headMessage->next)
+        { 
+          //if the message is one that we want to read
+          if (((headMessage->receiver) == (runningQueue->thread_id)) && (headMessage->sender == *tid))
+          {
+            //perform same operation as above TODO: consolidate this
+            strcpy(msg, headMessage->message);
+            *len = headMessage->len;
+            if (headMessage != NULL)
+            {
+              tmp = headMessage;
+              tmp->next = headMessage->next;
+              freemsg(headMessage); //prevent leak
+              break;
+            }
+          }
+          else
+          {
+            //iterate
+            headMessage = headMessage->next;
+          }
+        }
+      }
+    }
+  }
+}
+
+void freemsg(messageNode *msg)
+{
+  free(msg->message);
+  free(msg);
 }
